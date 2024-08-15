fe2360565ee1dde3f229bc7e67d578ddaa792bb8
1ster14ka <danyazybastik@gmail.com>
2024-08-15 18:43:21 +0300

    done tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,16 @@
-"use strict";
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  let amountCredits = customerCredits - quantity * pricePerDroid;
+  if (amountCredits < 0) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${
+      quantity * pricePerDroid
+    } credits!`;
+  }
 }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
